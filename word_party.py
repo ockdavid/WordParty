2dc03b30a61965cdcc2b24f60880822ce9e86c5e
David Landeo <david.landeo@pucp.edu.pe>
2023-09-06 00:54:11 +0200

    main

--- word_party.py
+++ word_party.py
@@ -8,6 +8,7 @@ from io import StringIO
 import time
 
 st.title('Word Party')
+st.markdown("Export any WhatsApp chat you desire and upload it below to visualize the most common words in a generated word cloud")
 
 # Upload the file to analyze
 uploaded_file = st.file_uploader("Upload file to analyze", type=['txt'])
